a43021821c868df1cfe5d2e929312e9cfce68ef4
S.P.Randeepa <96324718+Randipa@users.noreply.github.com>
2026-09-14 14:18:27 +0530

    fix register notification

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -3,6 +3,8 @@ import catalogPlugin from '@backstage/plugin-catalog/alpha';
 import catalogImportPlugin from '@backstage/plugin-catalog-import/alpha';
 import githubActionsPlugin from '@backstage-community/plugin-github-actions/alpha';
 import sonarqubePlugin from '@backstage-community/plugin-sonarqube/alpha';
+import notificationsPlugin from '@backstage/plugin-notifications/alpha';
+import signalsPlugin from '@backstage/plugin-signals/alpha';
 import { navModule } from './modules/nav';
 import { homeModule } from './modules/home';
 import { awsPlatformModule } from './modules/platform';
@@ -11,6 +13,8 @@ import { authModule } from './modules/auth';
 export default createApp({
   features: [
     authModule,
+    signalsPlugin,
+    notificationsPlugin,
     catalogPlugin,
     catalogImportPlugin,
     githubActionsPlugin,
